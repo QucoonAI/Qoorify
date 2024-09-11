7d3cdbed0b114ebedb6afbae8b9ebc2dcfc57a09
Ifioketuk <104146924+Ifioketuk@users.noreply.github.com>
2024-09-11 12:23:18 +0100

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -4,19 +4,17 @@ from src.anti_spoof import spoof_predict
 import os
 import time
 from tensorflow.keras.models import load_model
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration, VideoProcessorBase
+import av
+
 st.set_page_config(page_title="Streamlit WebCam App")
 
-
-st.cache_data()
+@st.cache_data()
 def load_anti_spoof_model():
     model = load_model("./model/face_antispoofing_model.h5")
     return model
 
 model = load_anti_spoof_model()
-
-
-cap = cv2.VideoCapture(1)
-frame_placeholder = st.empty()
 
 # Directory to save frames
 FRAME_DIR = "captured_frames"
@@ -35,29 +33,28 @@ def predict_spoof(file_paths):
 st.title("Live vs Spoof Detection")
 st.write("Look into the camera when prompted to verify you're not a spoof.")
 
-# Class to handle video processing from webcam
-class WebTest():
+class WebTest(VideoProcessorBase):
     def __init__(self):
         self.frames = []
         self.frame_paths = []
         self.current_step = "center"
-        self.instructions = {"center": "Look straight into the camera.",
-                             "right": "Turn your head to the right.",
-                             "left": "Turn your head to the left."}
+        self.instructions = {
+            "center": "Look straight into the camera.",
+            "right": "Turn your head to the right.",
+            "left": "Turn your head to the left."
+        }
         self.result_message = None
         self.frames_to_capture = 3
 
     def transform(self, frame):
-        # img = frame.to_ndarray(format="bgr24")
-        img = frame
+        img = frame.to_ndarray(format="bgr24")
 
         # Capture and save frames
         if len(self.frames) < self.frames_to_capture:
             st.info(self.instructions[self.current_step])
             self.frames.append(img)
             file_path = os.path.join(FRAME_DIR, f"{self.current_step}_{len(self.frames)}.jpg")
-            # cv2.imwrite(file_path, img)
-            cv2.imwrite(file_path,  cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+            cv2.imwrite(file_path, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
             self.frame_paths.append(file_path)
 
         # If enough frames are captured, run the model
@@ -84,24 +81,8 @@ class WebTest():
             st.success(self.result_message)
             self.result_message = None
 
-        return img
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 # Start webcam stream and apply the video transformer
-
-
 if st.button('Start verification'):
-    start = time.time()
-    web = WebTest()
-    
-    while cap.isOpened() and web.result_message!="Verification Complete. You're verified!":
-        ret, frame = cap.read()
-        frame = cv2.flip(frame, 1)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_placeholder.image(frame, channels="RGB")
-        print(time.time() - start)
-        if time.time() - start >= 5:
-            web.transform(frame)
-            start = time.time()
-    
-    # cap.release()
-    # cv2.destroyAllWindows()
+    webrtc_streamer(key='key', video_processor_factory=WebTest)
